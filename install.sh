2d0402926794142ccf4d4bc1af58b5350abbad53
Danny Lee <danny.lee@appian.com>
2018-03-07 23:08:17 -0500

    Added more commands

--- install.sh
+++ install.sh
@@ -12,3 +12,6 @@ echo "#### k8s-helper ####" >> ~/.bash_profile
 echo "alias k8='$SCRIPT/k8'" >> ~/.bash_profile
 echo "alias k8d='$SCRIPT/k8d'" >> ~/.bash_profile
 echo "alias k8ssh='$SCRIPT/k8ssh'" >> ~/.bash_profile
+echo "alias k8log='$SCRIPT/k8log'" >> ~/.bash_profile
+echo "alias k8del='$SCRIPT/k8del'" >> ~/.bash_profile
+echo "#### k8s-helper END ####" >> ~/.bash_profile
