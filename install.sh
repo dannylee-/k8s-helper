e3c4f93026d0cfbb5ced32348a4ddbcca12465b2
Danny Lee <danny.lee@appian.com>
2022-10-04 22:37:13 -0400

    Fixes

--- install.sh
+++ install.sh
@@ -12,9 +12,10 @@ echo "default" > namespace.mem
 echo "" >> ~/.bash_profile
 echo "#### k8s-helper ####" >> ~/.bash_profile
 echo "alias k8='$SCRIPT/k8'" >> ~/.bash_profile
+echo "alias k8get='$SCRIPT/k8get'" >> ~/.bash_profile
 echo "alias k8d='$SCRIPT/k8d'" >> ~/.bash_profile
 echo "alias k8e='$SCRIPT/k8e'" >> ~/.bash_profile
-echo "alias k8ssh='$SCRIPT/k8ssh'" >> ~/.bash_profile
+echo "alias k8exec='$SCRIPT/k8exec'" >> ~/.bash_profile
 echo "alias k8log='$SCRIPT/k8log'" >> ~/.bash_profile
 echo "alias k8del='$SCRIPT/k8del'" >> ~/.bash_profile
 echo "#### k8s-helper END ####" >> ~/.bash_profile
